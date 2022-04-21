b22c13a48df3266df92bd65f8ebbb585d2f6d659
LRed22 <lrdavid.com>
2022-04-20 20:51:26 -0400

    added CSS and JS to an html doc

--- JS_Script/externalSforevent.js
+++ JS_Script/externalSforevent.js
@@ -4,6 +4,23 @@ function message(msg){
 
 function displayDate(){
     document.getElementById("demo").innerHTML=Date()
-    displayDate:inline;
-    
+
+}
+
+function closeMe(){
+    // find the element
+    x=document.getElementById("demo");
+    //option 1: change the style attribute directly 
+    x.style.display="none";
+
+    //option 2: Change the class 
+    // x.className="closed"
+}
+
+function openMe(){
+    // Find the element
+    x.style.display="block";
+
+    //Option 2: change the class 
+    //x.className="open";
 }
