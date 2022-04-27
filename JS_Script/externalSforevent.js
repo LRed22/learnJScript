7459ccd3920fca4b61241bd61e44fe6e21ed1639
LRed22 <lrdavid.com>
2022-04-27 05:34:53 -0400

    added statement

--- JS_Script/externalSforevent.js
+++ JS_Script/externalSforevent.js
@@ -11,16 +11,16 @@ function closeMe(){
     // find the element
     x=document.getElementById("demo");
     //option 1: change the style attribute directly 
-    x.style.display="none";
+    //x.style.display="none";
 
     //option 2: Change the class 
-    // x.className="closed"
+    x.className="closed"
 }
 
 function openMe(){
     // Find the element
-    x.style.display="block";
+    //x.style.display="block";
 
     //Option 2: change the class 
-    //x.className="open";
+    x.className="open";
 }
